dbdf5d7f7738d3349f7cb7265c97cd68ed1e0757
Max Iudin <LmeMyu@yandex.ru>
2023-06-18 21:12:53 +0300

    sprint_2_commit

--- tests.py
+++ tests.py
@@ -1,24 +1,88 @@
+import pytest
+
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
+
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
-        collector = BooksCollector()
+    @pytest.fixture
+    def collector(self):
+        return BooksCollector()
 
-        # добавляем две книги
+    def test_add_new_book_add_two_books(self, collector):
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
-
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
         assert len(collector.get_books_rating()) == 2
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    def test_add_new_book_duplicate(self, collector):
+        collector.add_new_book('Война и мир')
+        collector.add_new_book('Война и мир')
+        assert collector.get_books_rating() == {'Война и мир': 1}
+
+    def test_set_book_rating_not_list_fails(self, collector):
+        collector.add_new_book('Война и мир')
+        collector.set_book_rating('Подросток', 2)
+        assert collector.get_books_rating() != {'Подросток': 2}
+
+    def test_set_book_rating_min_than_can(self, collector):
+        collector.add_new_book('Война и мир')
+        collector.set_book_rating('Война и мир', 0)
+        assert collector.get_books_rating() == {'Война и мир': 1}
+
+    def test_set_book_rating_max_than_can(self, collector):
+        collector.add_new_book('Война и мир')
+        collector.set_book_rating('Война и мир', 15)
+        assert collector.get_books_rating() == {'Война и мир': 1}
+
+    def test_set_book_rating(self, collector):
+        collector.add_new_book('Война и мир')
+        collector.set_book_rating('Война и мир', 7)
+        assert collector.books_rating == {'Война и мир': 7}
+
+    def test_get_book_rating_no_rate(self, collector):
+        collector.add_new_book('Война и мир')
+        rating = collector.get_book_rating('Подросток')
+        assert rating is None
+
+    def test_get_book_rating(self, collector):
+        collector.add_new_book('Война и мир')
+        assert collector.get_book_rating('Война и мир') == 1
+
+    def test_get_books_with_specific_rating(self, collector):
+        collector.add_new_book('Война и мир')
+        collector.add_new_book('Подросток')
+        collector.add_new_book('Дядя Ваня')
+        collector.set_book_rating('Война и мир', 9)
+        collector.set_book_rating('Подросток', 6)
+        collector.set_book_rating('Дядя Ваня', 9)
+        result = collector.get_books_with_specific_rating(9)
+        assert (['Война и мир', 'Дядя Ваня']) == result
+
+    def test_get_books_rating_list_full(self, collector):
+        collector.add_new_book('Война и мир')
+        collector.add_new_book('Подросток')
+        collector.add_new_book('Дядя Ваня')
+        assert collector.get_books_rating() != {}
+
+    def test_add_book_in_favorites(self, collector):
+        collector.add_new_book('Война и мир')
+        collector.add_book_in_favorites('Война и мир')
+        assert collector.books_rating == {'Война и мир': 1}
+
+    def test_add_book_in_favorites_not_list(self, collector):
+        collector.add_new_book('Война и мир')
+        collector.add_book_in_favorites('Подросток')
+        assert collector.books_rating != {'Подросток': 1}
+
+    def test_delete_book_from_favorites(self, collector):
+        collector.add_new_book('Война и мир')
+        collector.add_book_in_favorites('Война и мир')
+        collector.delete_book_from_favorites('Война и мир')
+        assert collector.books_rating == {'Война и мир': 1}
+
+    def test_get_list_of_favorites_books(self, collector):
+        collector.add_new_book('Война и мир')
+        collector.add_new_book('Дядя Ваня')
+        collector.add_book_in_favorites('Война и мир')
+        collector.add_book_in_favorites('Дядя Ваня')
+        assert collector.get_list_of_favorites_books() == ['Война и мир', 'Дядя Ваня']
